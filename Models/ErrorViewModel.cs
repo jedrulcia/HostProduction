69e8eb37f583df7d3678633652195a219135ceac
Jedrulcia <jedrzej.koriat@gmail.com>
2025-01-16 19:04:04 +0100

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HostProduction.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# HostProduction
+ASP.NET Core application made for educational purposes
